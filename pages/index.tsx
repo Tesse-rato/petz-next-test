fe667702befbc8826148ef61e500789885de7d3f
Pedro Henrique <phmartins.developer@gmail.com>
2023-09-26 14:34:08 -0300

    Criando a pagina home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import PokemonHero from "../public/images/pokemon-hero.jpg";
+
 export default function Home() {
-  return <>Estou na home</>;
+  return (
+    <div className={styles.container}>
+      <Image className={styles.backImage} src={PokemonHero} alt="PokemonHero" />
+      <span className={styles.text}>
+        Cuidamos bem do seu pokémon, para ele cuidar bem de você
+      </span>
+    </div>
+  );
 }
